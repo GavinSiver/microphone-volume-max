c145a3bd9cf6d9ffdab5e93ea60143c1ab43cd84
gavin <33779537+GavinSiver@users.noreply.github.com>
2024-06-27 23:07:27 -0500

    feat: stable release v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,32 @@
+use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-use windows::Win32::System::Com::*;
-use windows::Win32::{
-    Foundation::*,
-    Media::Audio::{
-        eCapture, eConsole, Endpoints::IAudioEndpointVolume, IMMDevice, IMMDeviceEnumerator,
-        MMDeviceEnumerator,
-    },
+use clap::Parser;
+use windows::Win32::Media::Audio::{
+    eCapture, eConsole, Endpoints::IAudioEndpointVolume, IMMDevice, IMMDeviceEnumerator,
+    MMDeviceEnumerator,
 };
+use windows::Win32::System::Com::*;
+
+#[derive(Parser)]
+#[command(name = "Windows Microphone Volume Max")]
+#[command(
+    version,
+    about = "Set the microphone volume to 100% on a recurring basis.",
+    long_about = "Finds the default microphone, and sets its volume to 100%; repeats this action every T seconds. Default is 60 seconds."
+)]
+struct Config {
+    /// Time in Seconds to wait before checking the volume again
+    #[arg(short, long, default_value = "60", value_parser = clap::value_parser!(u64).range(1..))]
+    time: u64,
+}
 
 pub fn start_windows_microphone_volume_max() -> Result<(), windows::core::Error> {
+    let config = Config::parse();
+
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
 
@@ -45,7 +58,7 @@ pub fn start_windows_microphone_volume_max() -> Result<(), windows::core::Error>
                 println!("Microphone volume set to 100%");
             }
 
-            thread::sleep(Duration::from_millis(1000));
+            thread::sleep(Duration::from_millis(config.time * 1000));
         }
 
         println!("Exiting...");
